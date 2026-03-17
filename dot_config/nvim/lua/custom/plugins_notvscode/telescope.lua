63dc754dad7a6f092396bf01cc0fa067acf4044f
Josh Hart <joshcolehart@gmail.com>
2026-03-16 21:55:34 -0700

    Update .config/nvim/init.lua Update .config/nvim/lazy-lock.json Update .config/nvim/lua/custom/plugins/auto-session.lua Update .config/nvim/lua/custom/plugins/fugitive.lua Update .config/nvim/lua/custom/plugins_notvscode/autocomplete.lua Update .config/nvim/lua/custom/plugins_notvscode/autoformat.lua Update .config/nvim/lua/custom/plugins_notvscode/lsp.lua Update .config/nvim/lua/custom/plugins_notvscode/telescope.lua Add .config/nvim/syntax/dafny.vim

--- dot_config/nvim/lua/custom/plugins_notvscode/telescope.lua
+++ dot_config/nvim/lua/custom/plugins_notvscode/telescope.lua
@@ -2,7 +2,8 @@ return {
   { -- Fuzzy Finder (files, lsp, etc)
     'nvim-telescope/telescope.nvim',
     event = 'VimEnter',
-    branch = '0.1.x',
+    branch = 'master',
+    commit = '5255aa27c422de944791318024167ad5d40aad20',
     dependencies = {
       'nvim-lua/plenary.nvim',
       { -- If encountering errors, see telescope-fzf-native README for installation instructions
